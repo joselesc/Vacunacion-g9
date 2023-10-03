798dababf9402f9cb301053cbfb8f88cf33313e5
Ched2370 <mmibz.85@gmail.com>
2023-10-03 20:14:51 -0300

    conectando main del paquete principal a mainVista

--- src/vacunacion/g9/VacunacionG9.java
+++ src/vacunacion/g9/VacunacionG9.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vacunacion.g9;
 
-/**
- *
- * @author josel
- */
+import vacunacion.g9.accesoADatos.Conexion;
+import vacunacion.g9.vistas.VacunacionG9Vista;
+
 public class VacunacionG9 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conexion.getConexion();
+        VacunacionG9Vista ingreso = new VacunacionG9Vista();
+        ingreso.main(args);
     }
     
 }
