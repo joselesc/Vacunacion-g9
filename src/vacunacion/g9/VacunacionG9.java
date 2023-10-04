bbebc0e0b596a123f42ed2863df66d18f479b596
Ched2370 <mmibz.85@gmail.com>
2023-10-04 18:21:13 -0300

    cargando mas iconos en carpeta

--- src/vacunacion/g9/VacunacionG9.java
+++ src/vacunacion/g9/VacunacionG9.java
@@ -6,9 +6,9 @@ import vacunacion.g9.vistas.VacunacionG9Vista;
 public class VacunacionG9 {
 
     public static void main(String[] args) {
-        Conexion.getConexion();
         VacunacionG9Vista ingreso = new VacunacionG9Vista();
         ingreso.main(args);
+        Conexion.getConexion();
     }
     
 }
--- src/vacunacion/g9/entidades/CitaVacunacion.java
+++ src/vacunacion/g9/entidades/CitaVacunacion.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vacunacion.g9.entidades;
 
 import java.time.LocalDateTime;
 
-/**
- *
- * @author josel
- */
 public class CitaVacunacion {
     
     private int codCita;
