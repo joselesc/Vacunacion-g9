bee98745ebe2f58e9b11f7cb1cf19cb64bbfee77
joselesc <joselescanogm@gmail.com>
2023-10-04 19:23:53 -0300

    cambios

--- src/vacunacion/g9/vistas/VacunacionG9Vista.java
+++ src/vacunacion/g9/vistas/VacunacionG9Vista.java
@@ -31,22 +31,13 @@ public class VacunacionG9Vista extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        ImageIcon icono=new ImageIcon(getClass().getResource("/vacunacion/g9/imagenes/vacunacion-covid-fullhd.png"));
+        ImageIcon icono=new ImageIcon(getClass().getResource("/vacunacion/g9/imagenes/fondo.png"));
         Image miImagen=icono.getImage();
         escritorio = new javax.swing.JDesktopPane(){
             public void paintComponent(Graphics g){
                 g.drawImage(miImagen,0,0,getWidth(),getHeight(),this);
             }
         };
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
-        jMenu3 = new javax.swing.JMenu();
-        jMenu4 = new javax.swing.JMenu();
-        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
-        jMenu5 = new javax.swing.JMenu();
-        jMenu6 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -60,58 +51,6 @@ public class VacunacionG9Vista extends javax.swing.JFrame {
             escritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 847, Short.MAX_VALUE)
         );
-
-        jMenu1.setBackground(new java.awt.Color(0, 0, 0));
-        jMenu1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenu1.setText("CIUDADANO");
-        jMenu1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
-        jMenuItem1.setText("jMenuItem1");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem1);
-
-        jMenuBar1.add(jMenu1);
-
-        jMenu2.setBackground(new java.awt.Color(204, 204, 255));
-        jMenu2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenu2.setText("LABORATORIO");
-        jMenu2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jMenuBar1.add(jMenu2);
-
-        jMenu3.setBackground(new java.awt.Color(204, 204, 255));
-        jMenu3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenu3.setText("APLICACION");
-        jMenu3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jMenuBar1.add(jMenu3);
-
-        jMenu4.setBackground(new java.awt.Color(204, 204, 255));
-        jMenu4.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenu4.setText("CENTRO DE VACUNACION");
-        jMenu4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
-        jCheckBoxMenuItem1.setSelected(true);
-        jCheckBoxMenuItem1.setText("jCheckBoxMenuItem1");
-        jMenu4.add(jCheckBoxMenuItem1);
-
-        jMenuBar1.add(jMenu4);
-
-        jMenu5.setBackground(new java.awt.Color(204, 204, 255));
-        jMenu5.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenu5.setText("CITA DE VACUNACION");
-        jMenu5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jMenuBar1.add(jMenu5);
-
-        jMenu6.setBackground(new java.awt.Color(204, 204, 255));
-        jMenu6.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenu6.setText("SALIR");
-        jMenu6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jMenuBar1.add(jMenu6);
-
-        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -131,14 +70,6 @@ public class VacunacionG9Vista extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-            escritorio.removeAll();
-            escritorio.repaint();
-            
-        
-        
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -177,14 +108,5 @@ public class VacunacionG9Vista extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane escritorio;
-    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu3;
-    private javax.swing.JMenu jMenu4;
-    private javax.swing.JMenu jMenu5;
-    private javax.swing.JMenu jMenu6;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
     // End of variables declaration//GEN-END:variables
 }
