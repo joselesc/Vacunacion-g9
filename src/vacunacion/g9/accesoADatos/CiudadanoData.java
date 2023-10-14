8db51c6921f144dea2c5e1d27c66de11f901f856
joselesc <joselescanogm@gmail.com>
2023-10-13 21:08:36 -0300

    metodo listar y borrar ciudadanos

--- src/vacunacion/g9/accesoADatos/CiudadanoData.java
+++ src/vacunacion/g9/accesoADatos/CiudadanoData.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
+import vacunacion.g9.entidades.Centro;
 import vacunacion.g9.entidades.Ciudadano;
 import vacunacion.g9.vistas.CiudadanoPrevioVista;
 
@@ -12,6 +15,7 @@ public class CiudadanoData {
 
     private Connection con = null;
     private int dniReg;
+
     public CiudadanoData() {
         con = Conexion.getConexion();
     }
@@ -31,7 +35,7 @@ public class CiudadanoData {
             ps.setString(6, ciudadano.getZona());
             ps.setString(7, ciudadano.getPatologia());
             ps.setBoolean(8, ciudadano.isAmbitoTrabajo());
-            ps.setBoolean(9,ciudadano.isRiesgo());
+            ps.setBoolean(9, ciudadano.isRiesgo());
             ps.executeUpdate();
             ps.close();
             JOptionPane.showMessageDialog(null, "Registro exitoso!!!te van a vacunar");
@@ -73,19 +77,18 @@ public class CiudadanoData {
     }
 
     public int loguearse(int dni) {
-        
-        int c=0;
+
+        int c = 0;
         String sql = "SELECT dni FROM ciudadano WHERE dni = ?";
 
         try (PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setInt(1, dni);
             try (ResultSet rs = ps.executeQuery()) {
-                
+
                 if (rs.next()) {
-                   
+
                     JOptionPane.showMessageDialog(null, "Ingreso exitoso");
-                    c=rs.getInt("dni");
-                   
+                    c = rs.getInt("dni");
 
                 } else {
                     JOptionPane.showMessageDialog(null, "No hay ninguna cuenta co esta dni, debe registrarse primero");
@@ -94,10 +97,68 @@ public class CiudadanoData {
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error de conexion -" + ex.getMessage());
         }
-        dniReg=c;
+        dniReg = c;
         return c;
 
     }
+
+    public List<Ciudadano> listarCiudadano() {
+
+        List<Ciudadano> ciu = new ArrayList<>();
+
+        try {
+            
+        
+            String sql = " select * from ciudadano ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                Ciudadano c = new Ciudadano();
+
+                c.setDni(rs.getInt("dni"));
+                c.setApellido(rs.getString("apellido"));
+                c.setNombre(rs.getString("nombre"));
+                c.setEmail(rs.getString("email"));
+                c.setCelular(rs.getInt("celular"));
+                c.setZona(rs.getString("zona"));
+                c.setPatologia(rs.getString("patologia"));
+                c.setAmbitoTrabajo(rs.getBoolean("ambitoTrabajo"));
+                c.setRiesgo(rs.getBoolean("deRiesgo"));
+                ciu.add(c);
+
+            }
+            ps.close();
+            rs.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Ciudadano" + ex.getMessage());
+        }
+
+        return ciu;
+    }
     
-   
-}
+    public void eliminarCiudadano(int dni){
+        
+        String sql= "DELETE FROM ciudadano WHERE dni=?";
+        
+         try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, dni);
+
+            int mod = ps.executeUpdate();
+
+            if (mod == 1) {
+                JOptionPane.showMessageDialog(null, "Ciudadano borrado.", "Mensaje", JOptionPane.PLAIN_MESSAGE);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "El dni del ciudadano no existe");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion -" + ex.getMessage());
+        }
+    }
+
+    }
